caf31679fb107f3abb4d05317995d956286ea44b
taeminers <taeminers@gmail.com>
2026-03-02 22:44:36 +0900

    hotfix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,10 +13,10 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Grids Agency",
+  title: "Grids Agency [그리즈 에이전시]",
   description: "서울대학교 컴퓨터공학부 출신 팀. AI 영상 제작부터 업무 효율을 극대화하는 AI 자동화 시스템 구축까지. 단순 웹사이트 제작을 넘어, 기업의 생산성을 혁신하는 맞춤형 내부 관리 툴과 워크플로우를 설계합니다. 그리즈 에이전시와 함께 비즈니스 운영의 초격차를 만드세요.",
   openGraph: {
-    title: "Grids Agency",
+    title: "Grids Agency [그리즈 에이전시]",
     description: "서울대학교 컴퓨터공학부 출신 팀. AI 영상 제작부터 업무 효율을 극대화하는 AI 자동화 시스템 구축까지. 단순 웹사이트 제작을 넘어, 기업의 생산성을 혁신하는 맞춤형 내부 관리 툴과 워크플로우를 설계합니다. 그리즈 에이전시와 함께 비즈니스 운영의 초격차를 만드세요.",
     images: [
       {
